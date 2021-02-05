8041eb9096ee8d27628be3e35ba9399f6f918c84
fritzgt <fritzgt@icloud.com>
2021-02-05 15:41:22 +0200

    Completed UI

--- NBC/ViewControllers/SearchViewController.swift
+++ NBC/ViewControllers/SearchViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.backgroundView = UIImageView(image: UIImage(named: "Wp18"))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
